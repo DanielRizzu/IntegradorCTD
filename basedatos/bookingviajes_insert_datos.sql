4e5e856db285a64c490b4ad9b25da01f5457cc56
DanielRizzu <sdaniel72@gmail.com>
2024-03-18 21:07:35 -0300

    Arreglo pequeño en BD

--- basedatos/bookingviajes_insert_datos.sql
+++ basedatos/bookingviajes_insert_datos.sql
@@ -111,7 +111,7 @@ insert into image values
     
     
 insert into user values
-    (1, 'Daniel', 'Rizzuti', 'daniel@viajes.com', '$2a$10$izo/rgH5EsHpxc43gOfXWeJThcD6mGnt88jm/LijDRdJGMay4x4Ba', 1, 1, 2),
+    (1, 'Super', 'Rizzu', 'daniel1213@viajes.com', '$2a$10$izo/rgH5EsHpxc43gOfXWeJThcD6mGnt88jm/LijDRdJGMay4x4Ba', 1, 1, 2),
     (2, 'Daniel', 'Rizzuti', 'daniel@viajes.com', '$2a$10$izo/rgH5EsHpxc43gOfXWeJThcD6mGnt88jm/LijDRdJGMay4x4Ba', 1, 1, 2),
     (3, 'Gonzalo', 'Pena', 'gonzaloP@viajes.com', '$2a$10$izo/rgH5EsHpxc43gOfXWeJThcD6mGnt88jm/LijDRdJGMay4x4Ba', 1, 1, 2),
     (4, 'Fernando', 'Conil', 'fernando@viajes.com', '$2a$10$izo/rgH5EsHpxc43gOfXWeJThcD6mGnt88jm/LijDRdJGMay4x4Ba', 1, 11,2),
