b2bf45dcab865f2709624f10863bb294ca045cd1
FJCon <conilfernando@gmail.com>
2024-04-02 14:31:06 -0300

    solucion error en etiquetas (usar jakarta, no javax)

--- backend/src/main/java/com/grupo6/bookingviajes/model/Favorite.java
+++ backend/src/main/java/com/grupo6/bookingviajes/model/Favorite.java
@@ -1,25 +1,29 @@
 package com.grupo6.bookingviajes.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.persistence.*;
 
 
-@Table(name = "favorite_product")
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
 @Setter
+
+@Entity
+@Table(name = "favorite_product")
+
 public class Favorite {
-@Id
-@ManyToOne
-@JoinColumn(name = "user_id")
-    private User user;
-@Id
-@ManyToOne
-@JoinColumn(name = "product_id")
+    @Id
+    @JoinColumn(name = "user_id")
+    private User usuario;
+    @Id
+    @JoinColumn(name = "product_id")
     private Product producto;
 }
